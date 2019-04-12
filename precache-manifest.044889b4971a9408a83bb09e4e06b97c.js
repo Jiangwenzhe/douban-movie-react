8c3f69519f64b11a4dee74e17835eee6e75aad7d
Erzhuyijian <1821344102@qq.com>
2019-04-12 21:01:45 +0800

    Updates

---
+++ precache-manifest.044889b4971a9408a83bb09e4e06b97c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1996d3c5870f1a477ee5",
+    "url": "/douban-movie-react/static/js/runtime~main.0425126c.js"
+  },
+  {
+    "revision": "7fb19ff8884524a013b6",
+    "url": "/douban-movie-react/static/js/main.5f994d54.chunk.js"
+  },
+  {
+    "revision": "2e64b3b50d8bd842032a",
+    "url": "/douban-movie-react/static/js/2.bb18f64b.chunk.js"
+  },
+  {
+    "revision": "7fb19ff8884524a013b6",
+    "url": "/douban-movie-react/static/css/main.39bd0301.chunk.css"
+  },
+  {
+    "revision": "527f205112c40b5a1c9077625c92fa59",
+    "url": "/douban-movie-react/index.html"
+  }
+];
